1c7022111b95ce0a0309a1d61304aefacb20f931
GitHub <actions@github.com>
2024-12-06 21:05:50 +0600

    project complete

--- Js/add-money.js
+++ Js/add-money.js
@@ -2,13 +2,18 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     event.preventDefault();
     const addMoney = getInputFieldValueById('add-money-input');
     const pinNumber = getInputFieldValueById('pin-number-input');
+    if(isNaN(addMoney)){
+        alert('failed to add money');
+        return;
+    }
     if(pinNumber === 1234){
         const balance = getTextFieldValueById('balance-amount');
         const newBalance = balance + addMoney
         document.getElementById('balance-amount').innerText = newBalance;
         // Transaction History
         const p = document.createElement('p');
-        p.innerText = `Add Money: ${addMoney} TK. Balance: ${newBalance} TK.`
+        p.innerText = `Add Money: ${addMoney}TK. Balance: ${newBalance}TK.`
+        p.classList.add('text-emerald-400');
         document.getElementById('transaction-container').appendChild(p);
     }else{
         alert('wrong input');
--- Js/cash-out.js
+++ Js/cash-out.js
@@ -2,9 +2,22 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
     event.preventDefault();
     const cashOut = getInputFieldValueById('cash-out-input');
     const pinNumber = getInputFieldValueById('pin-cash-out-input');
+    if(isNaN(cashOut)){
+        alert('failed to cash out');
+        return;
+    }
     if(pinNumber === 1234){
         const balance = getTextFieldValueById('balance-amount');
+        if(cashOut > balance){
+            alert('Low Balance');
+            return;
+        }
         const newBalance = balance - cashOut;
         document.getElementById('balance-amount').innerText = newBalance;
+        // Transaction History
+        const p = document.createElement('p');
+        p.innerText = `Cash Out: ${cashOut}TK. Balance: ${newBalance}TK.`
+        p.classList.add('text-red-400');
+        document.getElementById('transaction-container').appendChild(p);
     }
 })
